524cab68c07d86f01bf008be5d564b18593b4941
Andrewki44 <andrewkilbourn1996@gmail.com>
2026-03-21 02:10:00 +1100

    remove is exclusive

--- src/regions/omega_ruins.cs
+++ src/regions/omega_ruins.cs
@@ -6,7 +6,7 @@ internal unsafe static partial class Removers {
     private static class OmegaRuins {
         public static void remove_intro(byte* code_ptr)
         {
-            remove(code_ptr, 0x66D5, 0x68DB); // w1Ee02 - Still sets the OmegaRuinsProgressionFlags as normal after removal
+            remove(code_ptr, 0x66D5, 0x68DC); // w1Ee02 - Still sets the OmegaRuinsProgressionFlags as normal after removal
         }
     }
 }
