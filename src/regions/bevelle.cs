0cb0b487001aced92f7be600e09aa83776dde514
Andrewki44 <andrewkilbourn1996@gmail.com>
2026-03-23 23:30:07 +1100

    Updated remove end

--- src/regions/bevelle.cs
+++ src/regions/bevelle.cs
@@ -10,7 +10,7 @@ internal unsafe static partial class Removers {
             remove(code_ptr, 0x64ED, 0x665E);   // Set GameMoment = 2010 [07DAh];
             remove(code_ptr, 0x6664, 0x673C);   // Set GameMoment = 2060 [080Ch];
             remove(code_ptr, 0x6742, 0x694C);   // Set GameMoment = 2070 [0816h];
-            remove(code_ptr, 0x6959, 0x6A3F);   // Set GameMoment = 2075 [081Bh];
+            remove(code_ptr, 0x6959, 0x6A45);   // Set GameMoment = 2075 [081Bh];
             remove(code_ptr, 0x6A4B, 0x6D24);   // Set GameMoment = 2080 [0820h];
         }
     }
