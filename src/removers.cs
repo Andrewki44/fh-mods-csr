5ee3502e6c150af66cc503e843ad3c728e432389
Andrew K <53620706+Andrewki44@users.noreply.github.com>
2026-03-23 23:33:15 +1100

    SS Liki Departure (#9)

--- src/removers.cs
+++ src/removers.cs
@@ -16,6 +16,7 @@ internal unsafe static partial class Removers {
         //CutsceneRemoverModule.removers.Add("bsil0300", Besaid.remove_valley);
         //CutsceneRemoverModule.removers.Add("bsil0600", Besaid.remove_promontory);
         //CutsceneRemoverModule.removers.Add("bsil0700", Besaid.remove_village_slope);
+        CutsceneRemoverModule.removers.Add("bsil0100", Besaid.ss_liki_departs);
 
         CutsceneRemoverModule.removers.Add("bvyt0000", Bevelle.remove_intro);
 
