4465ffad5c6e611a25723cda0d5b8b644bf083c8
Andrew K <andrewkilbourn1996@gmail.com>
2026-03-31 12:29:28 +1100

    Pre & Post Sanctuary Keeper (WIP)

--- src/regions/mt_gagazet.cs
+++ src/regions/mt_gagazet.cs
@@ -4,6 +4,25 @@ namespace Fahrenheit.Mods.CSR;
 
 internal unsafe static partial class Removers {
     private static class MtGagazet {
+        public static void spectral_keeper(byte* code_ptr) {
+            // mtgz0800 (Gagazet - Summit Region)
+            remove(code_ptr, 0x357E, 0x3590); // "It comes!"
+            
+            // Camera for "It comes!"
+            remove(code_ptr, 0x35AC, 0x35B7);
+            remove(code_ptr, 0x3616, 0x3623);
 
+            remove(code_ptr, 0x36BA, 0x36CF); // Death Camera
+
+            remove(code_ptr, 0x3749, 0x38B0); // Post-kill Cutscene
+
+            // Battle Rewards Menu
+            // GameMoment = 2625
+            // run w0Be03
+
+            remove(code_ptr, 0x38E0, 0x3B7C); // Post Rewards Cutscene
+            // GameMoment = 2635
+            remove(code_ptr, 0x3B82, 0x3B8B);
+        }
     }
 }
--- src/removers.cs
+++ src/removers.cs
@@ -21,6 +21,8 @@ internal unsafe static partial class Removers {
 
         CutsceneRemoverModule.removers.Add("bvyt0000", Bevelle.remove_intro);
 
+        CutsceneRemoverModule.removers.Add("mtgz0800", MtGagazet.spectral_keeper);
+
         CutsceneRemoverModule.removers.Add("omeg0000", OmegaRuins.remove_intro);
 
     }
