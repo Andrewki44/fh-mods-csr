5c950dbb130fa4ed020b81ece1ed2b2752868883
Andrew K <53620706+Andrewki44@users.noreply.github.com>
2026-04-09 00:02:48 +1000

    Disabled Field Interaction and updates minimap (#18)
    * Disabled Field Interaction and updates minimap
    
    * Review comments

--- src/regions/baaj_temple.cs
+++ src/regions/baaj_temple.cs
@@ -7,11 +7,12 @@ internal unsafe static partial class Removers {
         public static void second_visit_dive(byte* code_ptr) {
             // bjyt0200 (Ruins - Underwater Hall)
 
-            remove(code_ptr, 0x9D17, 0x9D26); // W1Fcross - skip fadeout
-            remove(code_ptr, 0x9D38, 0x9D57);
+            remove(code_ptr, 0x9D17, 0x9D1D); // W1Fcross - skip fadeout
+            remove(code_ptr, 0x9D23, 0x9D26);
+            remove(code_ptr, 0x9D38, 0x9D41);
 
             // w1Be08
-            remove(code_ptr, 0x92A1, 0x92B2); // w0Ce45
+            remove(code_ptr, 0x92A1, 0x92A7); // call Common.wait [0000h](frames=5 [05h]);
 
             //// w00e1C- Common.warpToPoint and sets swimming movement etc
             remove(code_ptr, 0x0303, 0x035A);
@@ -21,8 +22,6 @@ internal unsafe static partial class Removers {
 
             remove(code_ptr, 0x92FE, 0x95AD); // "Payback time" 
             // Set saveData0381[0] = saveData0381[0] | 1 [01h];
-            
-            remove(code_ptr, 0x95BB, 0x9608); // Camera stuff
 
             // Next functions setup swimming party
             remove(code_ptr, 0x9683, 0x9689); // Don't need to fade in cause we never faded out
